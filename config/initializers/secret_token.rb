cbbe71f5374807e78e7fc926e22634ae60735b66
James Mac William <jmacwilliam@contantcontact.com>
2013-11-13 22:21:35 -0500

    generate shopify app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopifyRegistry::Application.config.secret_token = 'f9b6e69bef3609b527eca61918c7e4102613ab60bb58c059770c69882abc8654f2f10bc168def947fdfd5f566b9abe19e11d675956dfe1779d18991bb590f204'
